aa031e11444bd4a0ce8f0bac2bef91cda4f51526
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-07-01 23:50:37 +0800

    初始化

--- extend/spider/start.php
+++ extend/spider/start.php
@@ -1,3 +1,10 @@
 <?php
-file_get_contents('127.0.0.1');
-//file_get_contents('www.amazon.com');
+/*$url = '127.0.0.1';
+$content = file_get_contents($url);
+$content = json_decode($content, true);
+
+dump( $content);die;*/
+
+$fp = @fopen("C:/www/test.txt", "a+");
+fwrite($fp, "自动播报时间：\n" . date("Y-m-d H:i:s"));
+fclose($fp);
